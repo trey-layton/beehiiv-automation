918a75c5e28e19a02ed7a6199699fe404242532e
Trey Layton <laytontrey3@gmail.com>
2024-08-17 11:39:47 -0700

    Add content editor feature

---
+++ core/models/account_profile_service.py
@@ -0,0 +1,24 @@
+from pydantic import ValidationError
+
+from core.models.account_profile import AccountProfile
+from fastapi import HTTPException
+from supabase import Client
+
+
+class AccountProfileService:
+    def __init__(self, supabase_client: Client):
+        self.supabase = supabase_client
+
+    async def get_account_profile(self, account_profile: str):
+        response = (
+            self.supabase.table("account_profiles")
+            .select("*")
+            .eq("account_id", account_profile)
+            .execute()
+        )
+        if response.data:
+            try:
+                return AccountProfile(**response.data[0])
+            except ValidationError as e:
+                raise ValueError("Incomplete account profile data")
+        raise ValueError("Account profile not found")
