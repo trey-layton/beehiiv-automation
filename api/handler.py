9c528972206a163222cc0c95895cf3dab65352da
Trey Layton <laytontrey3@gmail.com>
2025-01-09 10:27:29 -0800

    added more logging to lifespan

--- api/handler.py
+++ api/handler.py
@@ -1,10 +1,29 @@
 import logging
-from api.main import app
+import json
 
 logger = logging.getLogger("vercel_app")
-logger.debug("=== Handler initialization ===")
+logger.debug("=== Pure ASGI Handler initialization ===")
 
 
-async def handler(scope, receive, send):
-    logger.debug(f"=== Handler called with scope: {scope} ===")
-    await app(scope, receive, send)
+async def app(scope, receive, send):
+    logger.debug(f"=== ASGI app called with scope type: {scope['type']} ===")
+
+    if scope["type"] == "http":
+        await send(
+            {
+                "type": "http.response.start",
+                "status": 200,
+                "headers": [
+                    [b"content-type", b"application/json"],
+                ],
+            }
+        )
+        await send(
+            {
+                "type": "http.response.body",
+                "body": json.dumps({"message": "Hello from minimal ASGI"}).encode(),
+            }
+        )
+
+
+handler = app
